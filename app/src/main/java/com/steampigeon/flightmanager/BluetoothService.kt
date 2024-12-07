4819ab07eaf29f04bb20d00795ae304571c651b5
kdschroer <89817557+kdschroer@users.noreply.github.com>
2024-12-07 10:50:57 -0800

    Daily commit

--- app/src/main/java/com/steampigeon/flightmanager/BluetoothService.kt
+++ app/src/main/java/com/steampigeon/flightmanager/BluetoothService.kt
@@ -4,11 +4,20 @@ import android.annotation.SuppressLint
 import android.app.Service
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
+import android.companion.AssociationInfo
+import android.companion.AssociationRequest
+import android.companion.BluetoothDeviceFilter
+import android.companion.CompanionDeviceManager
+import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -20,11 +29,33 @@ import kotlinx.coroutines.launch
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
+import java.util.regex.Pattern
 
 private const val TAG = "BluetoothService"
 private const val messageBufferSize = 256
 private const val prelaunchMessageSize = 74
 private const val telemetryMessageSize = 84
+
+enum class BluetoothState (val bluetoothState: UByte) {
+    NotStarted(0u),
+    Enabling(1u),
+    NotEnabled(2u),
+    NotSupported(3u),
+    Enabled(4u),
+    SelectingDevices(5u),
+    NoDevicesAvailable(6u),
+    Pairing(7u),
+    PairingFailed(8u),
+    Paired(9u),
+    Connected(10u),
+    ConnectionFailed(11u),
+    LostConnection(12u);
+
+    companion object {
+        fun fromUByte(value: UByte) = entries.firstOrNull { it.bluetoothState == value } ?: throw IllegalArgumentException("Invalid type: $value")
+    }
+
+}
 
 enum class MessageType (val messageType: UByte) {
     none(0u),
@@ -33,18 +64,6 @@ enum class MessageType (val messageType: UByte) {
 
     companion object {
         fun fromUByte(value: UByte) = entries.firstOrNull { it.messageType == value } ?: throw IllegalArgumentException("Invalid type: $value")
-    }
-}
-
-enum class ConnectionState (val connectionState: UByte) {
-    NotStarted(0u),
-    NoDevice(1u),
-    Connected(2u),
-    ConnectionFailed(3u),
-    LostConnection(4u);
-
-    companion object {
-        fun fromUByte(value: UByte) = entries.firstOrNull { it.connectionState == value } ?: throw IllegalArgumentException("Invalid type: $value")
     }
 }
 
@@ -58,100 +77,79 @@ class BluetoothService : Service() {
     private val _data = MutableSharedFlow<ByteArray>()
     val data: SharedFlow<ByteArray> = _data.asSharedFlow()
     private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-    var connectionState = ConnectionState.NotStarted
+    var bluetoothState: BluetoothState = BluetoothState.NotStarted
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy { BluetoothAdapter.getDefaultAdapter() }
+    private val locatorInputStream: InputStream = locatorSocket!!.inputStream
+    private val locatorOutputStream: OutputStream = locatorSocket!!.outputStream
+    //val bluetoothManager= context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    //bluetoothAdapter = bluetoothManager.adapter
+    private lateinit var enableBluetoothLauncher: ActivityResultLauncher<Intent>
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
 
     @SuppressLint("MissingPermission")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val locatorInputBuffer: ByteArray = ByteArray(messageBufferSize) // buffer store for the stream
-        var numBytes = 0 // bytes returned from read()
-        var headerReceived = false
-        var currentMessageSize = 0
-        var messageType = MessageType.none
-        val device = intent?.getParcelableExtra<BluetoothDevice>("device")
-        if (device != null && device.uuids != null) {
-            val locatorSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
-                device.createRfcommSocketToServiceRecord(device.uuids[0].uuid)
-            }
-            connectLocator(locatorSocket)
-            val locatorInputStream: InputStream = locatorSocket!!.inputStream
-            val locatorOutputStream: OutputStream = locatorSocket!!.outputStream
-
-            serviceScope.launch {
-                // Keep listening to the InputStream until an exception occurs.
-                while (true) {
-                    // Read from the InputStream.
-                    if (!headerReceived) {
-                        try {
-                            numBytes = locatorInputStream.read(locatorInputBuffer)
-                        } catch (e: IOException) {
-                            Log.d(TAG, "Input stream was disconnected", e)
-                            connectionState = ConnectionState.LostConnection
-                            numBytes = 0
-                            connectLocator(locatorSocket)
-                        }
-                        when {
-                            locatorInputBuffer.copyOfRange(0, 3).contentEquals(prelaunchMessageHeader) ->
-                                messageType = MessageType.prelaunch
-                            locatorInputBuffer.copyOfRange(0, 3).contentEquals(telemetryMessageHeader) ->
-                                messageType = MessageType.telemetry
-                        }
-                        if (messageType != MessageType.none) {
-                            headerReceived = true
-                            currentMessageSize = numBytes
-                        }
-                        //else
-                        //    locatorInputBuffer.fill(0)
+        //val device = intent?.getParcelableExtra<BluetoothDevice>("device")
+        serviceScope.launch {
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                when (bluetoothState) {
+                    BluetoothState.Connected -> {
+                        readReceiverData()
                     }
-                    else {
-                        try {
-                            numBytes = locatorInputStream.read(locatorInputBuffer, currentMessageSize, messageBufferSize - currentMessageSize)
-                            currentMessageSize += numBytes
-                        } catch (e: IOException) {
-                            Log.d(TAG, "Input stream was disconnected", e)
-                            connectionState = ConnectionState.LostConnection
-                            numBytes = 0
-                            connectLocator(locatorSocket)
+                    BluetoothState.LostConnection -> {
+                        connectLocator(locatorSocket)
+                    }
+                    BluetoothState.ConnectionFailed -> {
+                        delay (1000)
+                        connectLocator(locatorSocket)
+                    }
+                    BluetoothState.Enabled -> {
+                        if (locatorDevice?.bondState != BluetoothDevice.BOND_BONDED) {
+                            if (locatorDevice != null) {
+                                locatorDevice?.createBond()
+                                bluetoothState = BluetoothState.Pairing
+                            } else {
+                                selectBluetoothDevice(context)
+                            }
+                        }
+                        else
+                            bluetoothState = BluetoothState.Paired
+                    }
+                    BluetoothState.Pairing -> {
+                        bluetoothState = BluetoothState.Enabled
+                    }
+                    BluetoothState.SelectingDevices -> {
+                        if (bluetoothAdapter?.isDiscovering == false)
+                            bluetoothState = BluetoothState.Enabled
+                    }
+                    BluetoothState.Paired -> {
+                        if (bluetoothAdapter?.isEnabled == true) {
+                            if (locatorDevice?.bondState != BluetoothDevice.BOND_BONDED) {
+                                bluetoothState = BluetoothState.Enabled
+                            }
+                        }
+                        else {
+                            bluetoothState = BluetoothState.NotStarted
                         }
                     }
-                    if (numBytes != 0) {
-                        Log.d(TAG, logByteArrayAsChars(locatorInputBuffer, currentMessageSize))
-                        //Log.d(TAG, locatorInputBuffer.joinToString(", "))
+                    BluetoothState.PairingFailed -> {
+                        bluetoothState = BluetoothState.Enabled
                     }
-                    when {
-                        messageType == MessageType.prelaunch && currentMessageSize == prelaunchMessageSize -> {
-                            Log.d(TAG, "Prelaunch message detected, $currentMessageSize")
-                            _data.emit(locatorInputBuffer.copyOfRange(0, prelaunchMessageSize))
-                            numBytes = 0
-                            currentMessageSize = 0
-                            headerReceived = false
-                            messageType = MessageType.none
-                        }
-
-                        messageType == MessageType.telemetry && currentMessageSize == telemetryMessageSize -> {
-                            Log.d(TAG, "Telemetry message detected, $currentMessageSize")
-                            //_data.emit(locatorInputBuffer.copyOfRange(0, telemetryMessageSize))
-                            numBytes = 0
-                            currentMessageSize = 0
-                            headerReceived = false
-                            messageType = MessageType.none
-                        }
-
-                        currentMessageSize > telemetryMessageSize -> {
-                            Log.d(TAG, "Overflow detected, $currentMessageSize")
-                            //locatorInputBuffer.fill(0)
-                            numBytes = 0
-                            currentMessageSize = 0
-                            headerReceived = false
-                            messageType = MessageType.none
-                        }
-                    }
-                    //viewModel.updateData(mmBuffer)
-                    delay(100)
+                    BluetoothState.NotStarted ->
+                        enableBluetooth()
+                    else -> {}
+                    //}
+                    //else {
+                    //    bluetoothConnectionManager.RegisterReceiver(context)
+                    //}
                 }
+                delay(100)
             }
-        }
-        else {
-            connectionState = ConnectionState.NoDevice
         }
         return START_STICKY
     }
@@ -162,6 +160,77 @@ class BluetoothService : Service() {
         //serviceScope.cancel()
     }
 
+    fun enableBluetooth() {
+        when (bluetoothAdapter?.isEnabled) {
+            true -> bluetoothState = BluetoothState.Enabled
+            false -> {
+                val intent = Intent(this, EnablingActivity::class.java).apply { }
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                bluetoothState = BluetoothState.Enabling
+            }
+            null ->
+                bluetoothState = BluetoothState.NotSupported
+        }
+    }
+
+    fun selectBluetoothDevice(context: Context) {
+        val deviceManager = context.getSystemService(Context.COMPANION_DEVICE_SERVICE) as CompanionDeviceManager
+
+        // Create an association request
+        val deviceFilter: BluetoothDeviceFilter = BluetoothDeviceFilter.Builder()
+            .setNamePattern(Pattern.compile("RocketReceiver"))
+            .build()
+        val pairingRequest = AssociationRequest.Builder()
+            .addDeviceFilter(deviceFilter)
+            .setSingleDevice(false)
+            .build()
+
+        // Start pairing
+        deviceManager.associate(pairingRequest, object : CompanionDeviceManager.Callback() {
+            override fun onDeviceFound(chooserLauncher: IntentSender) {
+                //launcher.launch(
+                //    IntentSenderRequest.Builder(chooserLauncher).build()
+                //)
+                pairDevice()
+                //bluetoothConnectionState = BluetoothConnectionState.Pairing
+                Log.d(TAG, "onDeviceFound: ${chooserLauncher.toString()}")
+            }
+
+            override fun onAssociationPending(intentSender: IntentSender) {
+                super.onAssociationPending(intentSender)
+                Log.d(TAG, "onAssociationPending: ${intentSender.toString()}")
+            }
+
+            override fun onAssociationCreated(associationInfo: AssociationInfo) {
+                super.onAssociationCreated(associationInfo)
+                Log.d(TAG, "onAssociationCreated: ${associationInfo.toString()}")
+            }
+
+            override fun onFailure(error: CharSequence?) {
+                // Handle no devices found or "don't allow" user selection
+                bluetoothState = BluetoothState.PairingFailed
+                Log.d(TAG, "onFailure: $error")
+            }
+        }, null)
+        bluetoothState = BluetoothState.SelectingDevices
+        /*var lastExecutionTime = System.currentTimeMillis()
+        var elapsedSeconds = 0L
+        while (elapsedSeconds < 5000) {
+            elapsedSeconds = System.currentTimeMillis() - lastExecutionTime
+            delay(100)
+        }
+        if (bluetoothConnectionState != BluetoothConnectionState.Paired)
+            bluetoothConnectionState = BluetoothConnectionState.NotStarted*/
+    }
+
+    fun pairDevice() {
+        val intent = Intent(this, PairingActivity::class.java).apply {
+            //putExtra("device", device)
+        }
+        startActivity(intent)
+    }
+
     @SuppressLint("MissingPermission")
     fun connectLocator(locatorSocket: BluetoothSocket?) {
         val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
@@ -169,12 +238,100 @@ class BluetoothService : Service() {
         if (bluetoothAdapter?.isDiscovering == true)
             bluetoothAdapter.cancelDiscovery()
         // Attempt to connect to locator
+        if (device != null && device.uuids != null) {
+            val locatorSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+                device.createRfcommSocketToServiceRecord(device.uuids[0].uuid)
+            }
+            connectLocator(locatorSocket)
+        }
+        else {
+            bluetoothState = BluetoothState.NoDevicesAvailable
+        }
         try {
             locatorSocket?.connect()
-            connectionState = ConnectionState.Connected
+            bluetoothState = BluetoothState.Connected
         } catch (e: IOException) {
-            connectionState = ConnectionState.ConnectionFailed
+            bluetoothState = BluetoothState.ConnectionFailed
         }
+    }
+
+    fun readReceiverData() {
+        val locatorInputBuffer: ByteArray = ByteArray(messageBufferSize) // buffer store for the stream
+        var numBytes = 0 // bytes returned from read()
+        var headerReceived = false
+        var currentMessageSize = 0
+        var messageType = MessageType.none
+        // Read from the InputStream.
+        if (!headerReceived) {
+            try {
+                numBytes = locatorInputStream.read(locatorInputBuffer)
+            } catch (e: IOException) {
+                Log.d(TAG, "Input stream was disconnected", e)
+                bluetoothState = BluetoothState.LostConnection
+                numBytes = 0
+            }
+            when {
+                locatorInputBuffer.copyOfRange(0, 3)
+                    .contentEquals(prelaunchMessageHeader) ->
+                    messageType = MessageType.prelaunch
+
+                locatorInputBuffer.copyOfRange(0, 3)
+                    .contentEquals(telemetryMessageHeader) ->
+                    messageType = MessageType.telemetry
+            }
+            if (messageType != MessageType.none) {
+                headerReceived = true
+                currentMessageSize = numBytes
+            }
+            //else
+            //    locatorInputBuffer.fill(0)
+        } else {
+            try {
+                numBytes = locatorInputStream.read(
+                    locatorInputBuffer,
+                    currentMessageSize,
+                    messageBufferSize - currentMessageSize
+                )
+                currentMessageSize += numBytes
+            } catch (e: IOException) {
+                Log.d(TAG, "Input stream was disconnected", e)
+                bluetoothState = BluetoothState.LostConnection
+                numBytes = 0
+            }
+        }
+        if (numBytes != 0) {
+            Log.d(TAG, logByteArrayAsChars(locatorInputBuffer, currentMessageSize))
+            //Log.d(TAG, locatorInputBuffer.joinToString(", "))
+        }
+        when {
+            messageType == MessageType.prelaunch && currentMessageSize == prelaunchMessageSize -> {
+                Log.d(TAG, "Prelaunch message detected, $currentMessageSize")
+                _data.emit(locatorInputBuffer.copyOfRange(0, prelaunchMessageSize))
+                numBytes = 0
+                currentMessageSize = 0
+                headerReceived = false
+                messageType = MessageType.none
+            }
+
+            messageType == MessageType.telemetry && currentMessageSize == telemetryMessageSize -> {
+                Log.d(TAG, "Telemetry message detected, $currentMessageSize")
+                //_data.emit(locatorInputBuffer.copyOfRange(0, telemetryMessageSize))
+                numBytes = 0
+                currentMessageSize = 0
+                headerReceived = false
+                messageType = MessageType.none
+            }
+
+            currentMessageSize > telemetryMessageSize -> {
+                Log.d(TAG, "Overflow detected, $currentMessageSize")
+                //locatorInputBuffer.fill(0)
+                numBytes = 0
+                currentMessageSize = 0
+                headerReceived = false
+                messageType = MessageType.none
+            }
+        }
+        //viewModel.updateData(mmBuffer)
     }
 
     fun findHeader(messageBuffer: ByteArray, header: ByteArray): Int {
--- app/src/main/java/com/steampigeon/flightmanager/ui/LocatorSettingsScreen.kt
+++ app/src/main/java/com/steampigeon/flightmanager/ui/LocatorSettingsScreen.kt
@@ -1,26 +1,48 @@
 package com.steampigeon.flightmanager.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.steampigeon.flightmanager.R
 import com.steampigeon.flightmanager.ui.theme.FlightManagerTheme
 
@@ -43,8 +65,8 @@ fun LocatorSettingsScreen(
         modifier = modifier,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Column(modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))) {
-        }
+        NumericEntryWithButtons()
+        var number by remember { mutableStateOf(0) }
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -65,6 +87,84 @@ fun LocatorSettingsScreen(
                 onClick = onNextButtonClicked
             ) {
                 Text(stringResource(R.string.next))
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun NumberInputWithUpDown(
+    value: Int,
+    onValueChange: (Int) -> Unit,
+    minValue: Int = 0,
+    maxValue: Int = Int.MAX_VALUE
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Button(onClick = { onValueChange(value - 1) },
+            shape = RoundedCornerShape(topStart = 16.dp, bottomStart = 16.dp),
+            modifier = Modifier.combinedClickable(
+                onLongClick = {
+                    onValueChange(value - 5)
+                },
+                onClick = { onValueChange(value - 1) },
+            ),
+            enabled = value > minValue
+        ) {
+            Text(text = "-",
+                style = typography.titleMedium,)
+        }
+
+        TextField(
+            value = value.toString(),
+            onValueChange = {
+                val newValue = it.toIntOrNull() ?: value
+                if (newValue in minValue..maxValue) {
+                    onValueChange(newValue)
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.width(50.dp)
+        )
+
+        Button(onClick = { onValueChange(value + 1) }, enabled = value < maxValue) {
+            Text("+")
+        }
+    }
+}
+
+@Composable
+fun NumericEntryWithButtons() {
+    var value by remember { mutableStateOf("0") }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        OutlinedTextField(
+            value = value,
+            onValueChange = { newValue ->
+                value = if (newValue.isEmpty()) {
+                    "0"
+                } else {
+                    newValue.filter { it.isDigit() }
+                }
+            },
+            label = { Text("Launch Detect AGL") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.weight(1f)
+        )
+        //Spacer(modifier = Modifier.width(8.dp))
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            TextButton(onClick = { value = (value.toIntOrNull() ?: 0 + 1).toString() }) {
+                Icon(Icons.Outlined.KeyboardArrowUp, contentDescription = "Increment")
+            }
+            TextButton(onClick = { value = (value.toIntOrNull() ?: 0 - 1).toString() }) {
+                Icon(Icons.Outlined.KeyboardArrowDown, contentDescription = "Decrement")
             }
         }
     }
